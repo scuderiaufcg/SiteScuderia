8dc530da627230ddc00a6661f9df1793482f9461
svneto <severinoferreiracontato@gmail.com>
2025-09-18 01:58:00 -0300

    update: sponsors now up to date

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -6,13 +6,8 @@ export default function Sponsors() {
         <hr className="border-t border-blue-500 my-4" />
         <div className="flex flex-wrap justify-around items-center mb-8">
           <div className="w-1/2 p-2">
-            <a href="http://www.ciser.com.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/ciser.png" alt="Ciser" className="max-w-[342px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/2 p-2">
-            <a href="http://nutes.uepb.edu.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/nutes.png" alt="Nutes" className="max-w-[342px] mx-auto" />
+            <a href="https://acoindustrial.com.br/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-acoindustrial.png" alt="Aço Industrial" className="max-w-[342px] mx-auto" />
             </a>
           </div>
         </div>
@@ -21,9 +16,29 @@ export default function Sponsors() {
         <h3 className="text-[#FFD700] text-center text-xl">Ouro</h3>
         <hr className="border-t border-[#FFD700] my-4" />
         <div className="flex flex-wrap justify-around items-center mb-8">
+          <div className="w-1/4 p-2">
+            <a href="https://www.moura.com.br/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-moura.png" alt="Moura" className="max-w-[175px] mx-auto" />
+            </a>
+          </div>
           <div className="w-1/2 p-2">
-            <a href="http://stratusaeronaves.com.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/stratus.png" alt="Stratus" className="max-w-[250px] mx-auto" />
+            <a href="https://www.somangueiras.com/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-somangueiras.png" alt="Só Mangueiras" className="max-w-[342px] mx-auto" />
+            </a>
+          </div>
+          <div className="w-1/4 p-2">
+            <a href="http://www.paqtc.org.br/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-paqtcpb.png" alt="PAQTC" className="max-w-[175px] mx-auto" />
+            </a>
+          </div>
+          <div className="w-1/2 p-2">
+            <a href="http://www.ciser.com.br/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-ciser.svg" alt="Ciser" className="max-w-[342px] mx-auto" />
+            </a>
+          </div>
+          <div className="w-1/2 p-2">
+            <a href="http://nutes.uepb.edu.br/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-nutes.png" alt="Nutes" className="max-w-[342px] mx-auto" />
             </a>
           </div>
         </div>
@@ -33,28 +48,33 @@ export default function Sponsors() {
         <hr className="border-t border-gray-400 my-4" />
         <div className="flex flex-wrap justify-around items-center mb-8">
           <div className="w-1/4 p-2">
-            <a href="https://www.lojaacobrazil.com/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/acobrasil.png" alt="Aço Brazil" className="max-w-[200px] mx-auto" />
+            <a href="https://www.instagram.com/capotariadoivanildo_" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-capotaria.png" alt="Capotaria do Ivanildo" className="max-w-[200px] mx-auto" />
             </a>
           </div>
           <div className="w-1/4 p-2">
-            <a href="https://cdlcampina.org.br/site/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/cdl.png" alt="CDL" className="max-w-[200px] mx-auto" />
+            <a href="https://www.instagram.com/comercioindustrialgsi/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-gsi.png" alt="GSI" className="max-w-[200px] mx-auto" />
             </a>
           </div>
           <div className="w-1/4 p-2">
-            <a href="https://www.solidworks.com/pt-br" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/solidworks.png" alt="SolidWorks" className="max-w-[200px] mx-auto" />
+            <a href="https://www.instagram.com/_fempcar/?hl=en" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-fempcar.png" alt="FEMPCAR" className="max-w-[200px] mx-auto" />
             </a>
           </div>
           <div className="w-1/4 p-2">
+            <a href="https://www.instagram.com/varejaoautosservico/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-varejao.png" alt="Speedcar" className="max-w-[200px] mx-auto" />
+            </a>
+          </div>
+          <div className="w-1/4 p-2">
+            <a href="https://labsol-ufcg.github.io/site/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-labsol.png" alt="Labsol" className="max-w-[200px] mx-auto" />
+            </a>
+          </div>
+                    <div className="w-1/4 p-2">
             <a href="#" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/speedcar.png" alt="Speedcar" className="max-w-[200px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/4 p-2">
-            <a href="https://www.instagram.com/ubivelessa/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/ubivelessa.png" alt="Ubivelessa" className="max-w-[200px] mx-auto" />
+              <img src="/patrocinio/Patrocinadores/logo-altair.png" alt="Altair" className="max-w-[200px] mx-auto" />
             </a>
           </div>
         </div>
@@ -64,73 +84,32 @@ export default function Sponsors() {
         <hr className="border-t border-yellow-700 my-4" />
         <div className="flex flex-wrap justify-around items-center mb-8">
           <div className="w-1/4 p-2">
-            <a href="https://www.facebook.com/AssistemEscapamentos" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/assistem.png" alt="Assistem" className="max-w-[175px] mx-auto" />
+            <a href="https://www.solidworks.com/pt-br" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-solidworks.png" alt="Solid Works" className="max-w-[175px] mx-auto" />
             </a>
           </div>
           <div className="w-1/4 p-2">
-            <a href="http://www.atecel.org.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/atecel.png" alt="Atecel" className="max-w-[175px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/4 p-2">
-            <a href="http://www.hbroland.com.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/hb.png" alt="HB" className="max-w-[175px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/4 p-2">
-            <a href="https://www.moura.com.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/moura.png" alt="Moura" className="max-w-[175px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/4 p-2">
-            <a href="https://www.google.com/maps/place/Lubritech+Autocenter/@-7.253618,-35.8946463,15z" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/LubriTech.png" alt="Lubritech" className="max-w-[175px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/4 p-2">
-            <a href="http://www.paqtc.org.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/paq.png" alt="PAQTC" className="max-w-[175px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/4 p-2">
-            <a href="https://www.rocketperformance.com.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Patrocinadores/rocketPerformance.png" alt="Rocket Performance" className="max-w-[175px] mx-auto" />
-            </a>
-          </div>
-        </div>
-
-        {/* Apoiadores */}
-        <h3 className="text-black text-center text-xl">Apoiadores</h3>
-        <hr className="border-t border-black my-4" />
-        <div className="flex flex-wrap justify-around items-center">
-          <div className="w-1/4 p-2">
-            <a href="https://www.facebook.com/aldiesel.oficina/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Apoiadores/aldiesel.png" alt="Aldiesel" className="max-w-[150px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/4 p-2">
-            <a href="http://www.campinafm.com.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Apoiadores/campinafm.png" alt="Campina FM" className="max-w-[150px] mx-auto" />
-            </a>
-          </div>
-          <div className="w-1/4 p-2">
-            <a href="http://www.pjmtelecom.com/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Apoiadores/pjm.png" alt="PJM" className="max-w-[150px] mx-auto" />
+            <a href="https://www.acobrazil.com.br/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-acobrazil.png" alt="Aço Brazil" className="max-w-[175px] mx-auto" />
             </a>
           </div>
           <div className="w-1/4 p-2">
             <a href="#" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Apoiadores/projemaq.png" alt="PROJEMAQ" className="max-w-[150px] mx-auto" />
+              <img src="/patrocinio/Patrocinadores/logo-negaodoscape.png" alt="Negão do Scape" className="max-w-[175px] mx-auto" />
             </a>
           </div>
           <div className="w-1/4 p-2">
-            <a href="http://3arocket.com.br/" target="_blank" rel="noopener noreferrer">
-              <img src="/patrocinio/Apoiadores/rocket.png" alt="Rocket" className="max-w-[150px] mx-auto" />
+            <a href="https://www.instagram.com/old_garage_83/" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-83garage.png" alt="83 Garage" className="max-w-[175px] mx-auto" />
+            </a>
+          </div>
+
+          <div className="w-1/4 p-2">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <img src="/patrocinio/Patrocinadores/logo-simpetro.png" alt="SimPetro" className="max-w-[175px] mx-auto" />
             </a>
           </div>
         </div>
-
         {/* Contato */}
         <div className="mt-8 text-center">
           <h2 className="text-xl font-semibold mb-4">Ajude a equipe você também</h2>
